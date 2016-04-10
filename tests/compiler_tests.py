7c0bcf1b213bc41007a0bc4ecf08ca32e70f804a
radx64 <radekteam@gmail.com>
2016-04-10 15:54:47 +0200

    Some more compiler tests

--- tests/compiler_tests.py
+++ tests/compiler_tests.py
@@ -10,3 +10,23 @@ class CpuTests(unittest.TestCase):
         sourceCode = ""
         binary = self.compiler.compile(sourceCode)
         self.assertEquals(binary, [])
+
+    def test_IfCouldDecodeRegisters(self):
+        sourceCode = "MOV R0, R1"
+        binary = self.compiler.compile(sourceCode)
+        self.assertEquals(binary, [0x00, 0x00, 0x01])
+
+    def test_IfItHandleLabels(self):
+        sourceCode = ("start:\n"
+            "MOV R0, R1\n")
+
+        binary = self.compiler.compile(sourceCode)
+        self.assertIn('start', self.compiler.labels)      
+
+    def test_IfItHandleLabelsDeclaredAfterUse(self):
+        sourceCode = ("start:\n"
+            "JMP end\n"
+            "end:\n")
+
+        binary = self.compiler.compile(sourceCode)
+        self.assertEquals(binary, [0x40, 0x01])
